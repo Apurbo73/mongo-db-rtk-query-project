8561c205f8467684fbb173c0b2aed085298720c5
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-20 03:12:27 +0600

    not completed

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import productRoutes from "./router/productRoutes.js";
 import orderRoutes from "./router/productRoutes.js"
+import bodyParser from "body-parser";
 const app = express();
 const PORT = 3000;
-app.use("/orders",orderRoutes)
+app.use(bodyParser.json());
+app.use("/orders",orderRoutes);
 app.use("/products", productRoutes);
 app.get("/", (req, res) => {
   res.send("<h2>hi broh</h2>");
 });
-app.listen(PORT, () => {
+app.listen(PORT, () => { 
   console.log("first");
 });
--- router/orderRoutes.js
+++ router/orderRoutes.js
@@ -1,11 +1,14 @@
 import { express } from "express";
+import { getOrder,createOrder } from "../database/orders";
 const router = express.Router();
 router.get("/:reference", (req, res) => {
   res.send(`Getting orders with reference ${req.params.reference}`);
 });
 
 // create order endpoint:
-router.post('/', (req,res)=>{
-    res.send('Creating an order')
+router.post('/', async(req,res)=>{
+  const orderDate= req.body;
+  const newOrder= await createOrder(orderDate)
+    res.status(201).send({status:'OK', data:newOrder})
 })
 export default router;
